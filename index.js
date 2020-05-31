ec75049a3f8afa90a288634507ee224a1ee6573c
doncsoo <nanoxs10@gmail.com>
2020-05-31 20:31:29 +0200

    plssssss

--- index.js
+++ index.js
@@ -2,39 +2,30 @@ const express = require('express')
 const { Client, Pool } = require('pg');
 var app = express()
 
-queryDatabase = async(query) =>
-{
-    const pool = new Pool({
-        connectionString: process.env.DATABASE_URL
-    });
-     
-    await pool.connect()
-       .then(client => {
-        return client
-          .query(query)
-          .then(res => {
-            client.release()
-            console.log(res.rows);
-            return res.rows;
-          })
-          .catch(err => {
-            client.release()
-            console.log(err.stack)
-            return null;
-          })
-      })
-    
-    await pool.end()
-}
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
 
 app.get('/', function(req, res){
     res.send("Backend teszt! Egyenlőre csak database testing.");
  });
 
 app.get('/testsql', async function(req,res){
-    await queryDatabase('SELECT * FROM test')
-    .then(r => console.log(r))
-    .then(r => res.send(JSON.stringify(r)));
+    await pool.connect()
+       .then(client => {
+        return client
+          .query('SELECT * FROM test')
+          .then(res => {
+            client.release()
+            console.log(res.rows);
+          })
+          .then(r => console.log(r))
+          .then(r => res.send(JSON.stringify(r)))
+          .catch(err => {
+            client.release()
+            console.log(err.stack)
+          })
+      })
 });
 
 app.listen(process.env.PORT || 3000);
