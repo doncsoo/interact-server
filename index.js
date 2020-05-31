7e32fed1a3f52ed59069af3d4c697e88625719ba
doncsoo <nanoxs10@gmail.com>
2020-05-31 18:27:14 +0200

    adding postgresql query test

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const { Client } = require('pg');
 var app = express()
+
+queryDatabase(query)
+{
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      });
+      
+      client.connect();
+      
+      client.query(query, (err, res) => {
+        if (err) throw err;
+        /*for (let row of res.rows) {
+          console.log(JSON.stringify(row));
+        }*/
+        return JSON.stringify(res.rows);
+        client.end();
+      });
+}
 
 app.get('/', function(req, res){
     res.send("Backend teszt! Egyenlőre csak database testing.");
  });
+
+app.get('/testsql', function(req,res){
+    res.send(queryDatabase('SELECT * FROM test'));
+});
 
 app.listen(process.env.PORT || 3000)
